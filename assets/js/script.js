1589d7280467e86dd9c211a7b9976d2cca935398
Tyler Hance <tyler.hance@gmail.com>
2021-04-28 16:28:50 -0700

    Adding functions for past, present, future status in js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,63 @@
-var eventTextEl = "";
-var eventTimeEl = "";
+var appointmentTextEl = "";
+var appointmentTimeEl = "";
 var currentDate;
 var currentTime;
 var currentContainer;
 var tempArr = [];
-var storedEvents;
-var returnedEvents;
+var storedAppointment;
+var returnedAppointment;
 
 $(window).on("load", function () {
-    currentDate = moment().format('LLLL');
-    $("#currentDay").append(currentDate);
-})
+  currentDate = moment().format("LLLL");
+  $("#currentDay").append(currentDate);
+});
 
-// 
-function renderEvents(){
-    storedEvents = JSON.parse(localStorage.getItem("events"));
-    if (storedEvents!== null) {
-        for (i = 0; i < storedEvents.length; i++) {
-            returnedEvents = storedEvents[i];
-            details = returnedEvents.details;
-            timeIndex = returnedEvents.time;
-            timeIndex = timeIndex.replace(":00", "");
-            if (details !== null) {
-                $('#' + timeIndex).children("div").children("textarea").val(details);
-            }
-        }
+//
+function renderEvents() {
+  storedAppointment = JSON.parse(localStorage.getItem("events"));
+  if (storedAppointment !== null) {
+    for (i = 0; i < storedAppointment.length; i++) {
+      returnedAppointment = storedAppointment[i];
+      details = returnedAppointment.details;
+      timeIndex = returnedAppointment.time;
+      timeIndex = timeIndex.replace(":00", "");
+      if (details !== null) {
+        $("#" + timeIndex)
+          .children("div")
+          .children("div")
+          .children("textarea")
+          .val(details);
+      }
     }
+  }
 }
+
+renderEvents();
+
+for (i = 0; i <= 23; i++) {
+  currentContainer = i;
+  if (currentTime == i) {
+    $("#" + currentContainer).addClass("present");
+    $("#" + currentContainer)
+      .children("div")
+      .children("div")
+      .children("textarea")
+      .addClass("present");
+  } else if (currentTime > i) {
+    $("#" + currentContainer).addClass("past");
+    $("#" + currentContainer)
+      .children("div")
+      .children("div")
+      .children("textarea")
+      .addClass("past");
+  } else {
+    $("#" + currentContainer).addClass("future");
+    $("#" + currentContainer)
+      .children("div")
+      .children("div")
+      .children("textarea")
+      .addClass("future");
+  }
+}
+
+
