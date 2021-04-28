3c58bd5543b05e1226393a09775a8de439caec75
Tyler Hance <tyler.hance@gmail.com>
2021-04-28 15:31:58 -0700

    Working through the logic for storing events in specific time slot.

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,18 @@ $(window).on("load", function () {
     $("#currentDay").append(currentDate);
 })
 
+// 
+function renderEvents(){
+    storedEvents = JSON.parse(localStorage.getItem("events"));
+    if (storedEvents!== null) {
+        for (i = 0; i < storedEvents.length; i++) {
+            returnedEvents = storedEvents[i];
+            details = returnedEvents.details;
+            timeIndex = returnedEvents.time;
+            timeIndex = timeIndex.replace(":00", "");
+            if (details !== null) {
+                $('#' + timeIndex).children("div").children("textarea").val(details);
+            }
+        }
+    }
+}
